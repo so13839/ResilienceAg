bb0e07454634bf45b7e8960f53e7431f09fd223b
Stephen Opiyo <so13839@gmail.com>
2023-09-10 10:34:23 -0400

    Add files via upload

--- County-others.R
+++ County-others.R
@@ -32,3 +32,47 @@ ui <- fluidPage(
        
     )
   )
+
+
+server <- function(input, output) {
+  selected_county <- reactive({
+    subset(data1, County == input$County)$Value
+  })
+  
+  output$gauge1 <- renderGauge({
+    gauge(min = 0, 
+          max = 3, 
+          sectors = gaugeSectors(success = c(2.1, 3.0), 
+                                 warning = c(1.1, 2.0),
+                                 danger = c(0.0, 1.0)),
+          value = selected_county())
+  })
+  
+  selected_subcounty <- reactive({
+    subset(data2, Subcounty == input$Subcounty)$Value
+  })
+  
+  output$gauge2 <- renderGauge({
+    gauge(min = 0, 
+          max = 3, 
+          sectors = gaugeSectors(success = c(2.1, 3.0), 
+                                 warning = c(1.1, 2.0),
+                                 danger = c(0.0, 1.0)),
+          value = selected_subcounty())
+  })
+  
+  selected_ward <- reactive({
+    subset(data3, Ward == input$Ward)$Value
+  })
+  
+  output$gauge3 <- renderGauge({
+    gauge(min = 0, 
+          max = 3, 
+          sectors = gaugeSectors(success = c(2.1, 3.0), 
+                                 warning = c(1.1, 2.0),
+                                 danger = c(0.0, 1.0)),
+          value = selected_ward())
+  })
+}
+
+shinyApp(ui = ui, server = server)
--- server.R
+++ server.R
@@ -1,41 +1,54 @@
 library(shiny)
-model <- readRDS("model.RDS")
-# Server
+library(flexdashboard)
+library(readr)
+library(shinyWidgets)
+#library(echarts4r)
+
+# Sample data frame with location-value mapping
+data1 <- read_csv("data1.csv")
+data2 <- read_csv("data2.csv")
+data3 <- read_csv("data3.csv")
+
+
 server <- function(input, output) {
-  output$yield_plot <- renderPlot({
-    plot(data$yield ~ data$rainfall,
-         xlab = "Rainfall",
-         ylab = "Yield",
-         main = "Yield vs. Rainfall",
-         col = "blue",
-         pch = 19)
+  selected_county <- reactive({
+    subset(data1, County == input$County)$Value
   })
   
-  predicted_yield <- eventReactive(input$predict_button, {
-    new_data <- data.frame(rainfall = input$rainfall_input, nevi = input$nevi_input)
-    predict(model, new_data)
+  output$gauge1 <- renderGauge({
+    gauge(min = 0, 
+          max = 3, 
+          sectors = gaugeSectors(success = c(2.1, 3.0), 
+                                 warning = c(1.1, 2.0),
+                                 danger = c(0.0, 1.0)),
+          value = selected_county())
   })
   
-  output$yield_output <- renderText({
-    year <- input$year_input
-    yield_value <- predicted_yield()
-    paste("Yield for", year, ":", round(yield_value, 2), "Kg")
+  selected_subcounty <- reactive({
+    subset(data2, Subcounty == input$Subcounty)$Value
   })
   
-  output$score_output <- renderText({
-    year <- input$year_input
-    yield_value <- predicted_yield()
-    score <- ifelse(yield_value < 1000, "Bad",
-                    ifelse(yield_value >= 1000 && yield_value <= 1299, "Fair",
-                           ifelse(yield_value >= 1300 && yield_value <= 1799, "Good",
-                                  ifelse(yield_value >= 1800, "Excellent", ""))))
-    paste("Score for", year, ":", score)
+  output$gauge2 <- renderGauge({
+    gauge(min = 0, 
+          max = 3, 
+          sectors = gaugeSectors(success = c(2.1, 3.0), 
+                                 warning = c(1.1, 2.0),
+                                 danger = c(0.0, 1.0)),
+          value = selected_subcounty())
+  })
+  
+  selected_ward <- reactive({
+    subset(data3, Ward == input$Ward)$Value
+  })
+  
+  output$gauge3 <- renderGauge({
+    gauge(min = 0, 
+          max = 3, 
+          sectors = gaugeSectors(success = c(2.1, 3.0), 
+                                 warning = c(1.1, 2.0),
+                                 danger = c(0.0, 1.0)),
+          value = selected_ward())
   })
 }
 
-# Create a linear regression model
-#model <- lm(yield ~ rainfall + nevi, data = data)
-
-
-# Run the app
-#shinyApp(ui, server)
+shinyApp(ui = ui, server = server)
